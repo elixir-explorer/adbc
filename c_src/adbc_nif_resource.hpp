3f7a4fef420ff5e032b09c7fd83cea9b8d78a877
Marten/Qqwy <w-m@resilia.nl>
2023-06-05 09:54:27 +0200

    Use reinterpret_cast to cast res_type** to void**

--- c_src/adbc_nif_resource.hpp
+++ c_src/adbc_nif_resource.hpp
@@ -26,7 +26,7 @@ struct NifRes {
 
     static res_type * get_resource(ErlNifEnv * env, ERL_NIF_TERM term, ERL_NIF_TERM &error) {
         res_type * self_res = nullptr;
-        if (!enif_get_resource(env, term, res_type::type, static_cast<void **>(&self_res)) || self_res == nullptr) {
+        if (!enif_get_resource(env, term, res_type::type, reinterpret_cast<void **>(&self_res)) || self_res == nullptr) {
             error = erlang::nif::error(env, "cannot access Nif resource");
         }
 

6923d7afc4c07c8d8b7cb1f2d0710373f4e86e1c
José Valim <jose.valim@dashbit.co>
2023-06-14 22:42:58 +0200

    bind tests

--- test/adbc_statement_test.exs
+++ test/adbc_statement_test.exs
@@ -6,75 +6,68 @@ defmodule Adbc.Statement.Test do
   alias Adbc.Database
   alias Adbc.Statement
 
-  test "new statement and release it" do
+  setup do
     {:ok, %Database{} = database} = Database.new()
-    assert is_reference(database.reference)
     :ok = Database.set_option(database, "driver", "adbc_driver_sqlite")
-
-    assert :ok == Database.init(database)
+    :ok = Database.init(database)
 
     {:ok, %Connection{} = connection} = Connection.new()
-    assert is_reference(connection.reference)
+    :ok = Connection.init(connection, database)
 
-    assert :ok == Connection.init(connection, database)
-    {:ok, %Statement{} = statement} = Statement.new(connection)
-    assert is_reference(statement.reference)
+    on_exit(fn ->
+      Connection.release(connection)
+      Database.release(database)
+    end)
 
-    assert :ok == Statement.release(statement)
-
-    assert :ok == Connection.release(connection)
-    assert :ok == Database.release(database)
+    %{database: database, connection: connection}
   end
 
-  test "release a statement twice should raise an ArgumentError" do
-    {:ok, %Database{} = database} = Database.new()
-    assert is_reference(database.reference)
-    :ok = Database.set_option(database, "driver", "adbc_driver_sqlite")
-
-    assert :ok == Database.init(database)
-
-    {:ok, %Connection{} = connection} = Connection.new()
-    assert is_reference(connection.reference)
-
-    assert :ok == Connection.init(connection, database)
+  test "new statement and release it", %{connection: connection} do
     {:ok, %Statement{} = statement} = Statement.new(connection)
     assert is_reference(statement.reference)
+    assert :ok == Statement.release(statement)
+  end
 
+  test "release a statement twice should raise an ArgumentError", %{connection: connection} do
+    {:ok, %Statement{} = statement} = Statement.new(connection)
+    assert is_reference(statement.reference)
     assert :ok == Statement.release(statement)
     assert {:error, "invalid state"} == Statement.release(statement)
-
-    assert :ok == Connection.release(connection)
-    assert :ok == Database.release(database)
   end
 
-  test "execute statements" do
-    {:ok, %Database{} = database} = Database.new()
-    assert is_reference(database.reference)
-    :ok = Database.set_option(database, "driver", "adbc_driver_sqlite")
-
-    assert :ok == Database.init(database)
-
-    {:ok, %Connection{} = connection} = Connection.new()
-    assert is_reference(connection.reference)
-
-    assert :ok == Connection.init(connection, database)
+  test "execute statements", %{connection: connection, test: test} do
     {:ok, %Statement{} = statement} = Statement.new(connection)
     assert is_reference(statement.reference)
-    assert :ok == Statement.set_sql_query(statement, "CREATE TABLE foo (col)")
-    assert :ok == Statement.prepare(statement)
-
+    assert :ok == Statement.set_sql_query(statement, "CREATE TABLE \"#{test}\" (col)")
     {:ok, _stream, row_affected} = Statement.execute_query(statement)
     assert row_affected == -1
 
-    assert :ok == Statement.set_sql_query(statement, "INSERT INTO foo VALUES (42)")
-    assert :ok == Statement.prepare(statement)
-
+    assert :ok == Statement.set_sql_query(statement, "INSERT INTO \"#{test}\" VALUES (42)")
     {:ok, _stream, row_affected} = Statement.execute_query(statement)
     assert row_affected == -1
 
-    assert :ok == Statement.release(statement)
+    assert :ok == Statement.set_sql_query(statement, "INSERT INTO \"#{test}\" VALUES (43)")
+    assert :ok == Statement.prepare(statement)
+    {:ok, _stream, row_affected} = Statement.execute_query(statement)
+    assert row_affected == -1
+  end
 
-    assert :ok == Connection.release(connection)
-    assert :ok == Database.release(database)
+  test "bind statement", %{connection: connection, test: test} do
+    {:ok, %Statement{} = statement} = Statement.new(connection)
+    assert is_reference(statement.reference)
+    assert :ok == Statement.set_sql_query(statement, "CREATE TABLE \"#{test}\" (col)")
+    {:ok, _stream, row_affected} = Statement.execute_query(statement)
+    assert row_affected == -1
+
+    assert :ok == Statement.set_sql_query(statement, "INSERT INTO \"#{test}\" VALUES (?)")
+    assert :ok == Statement.bind(statement, [42])
+    {:ok, _stream, row_affected} = Statement.execute_query(statement)
+    assert row_affected == -1
+
+    assert :ok == Statement.set_sql_query(statement, "INSERT INTO \"#{test}\" VALUES (?)")
+    assert :ok == Statement.bind(statement, [3])
+    assert :ok == Statement.prepare(statement)
+    {:ok, _stream, row_affected} = Statement.execute_query(statement)
+    assert row_affected == -1
   end
 end

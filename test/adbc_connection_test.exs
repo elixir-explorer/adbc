9e26e077d7ffc352b65541e2b93a7ddbb3ff1f6f
Cocoa <i@uwucocoa.moe>
2023-07-08 12:06:38 +0800

    include the test for get_objects

--- test/adbc_connection_test.exs
+++ test/adbc_connection_test.exs
@@ -84,10 +84,59 @@ defmodule Adbc.Connection.Test do
   end
 
   describe "get_objects" do
-    @tag skip: "fails with bad tag"
     test "get all objects from a connection", %{db: db} do
       conn = start_supervised!({Connection, database: db})
-      {:ok, :done} = Connection.get_objects(conn, 0)
+
+      {:ok,
+       %Adbc.Result{
+         num_rows: nil,
+         data: %{
+           "catalog_db_schemas" => [
+             {"db_schema_name", []},
+             {"db_schema_tables",
+              [
+                {"table_name", []},
+                {"table_type", []},
+                {"table_columns",
+                 [
+                   {"column_name", []},
+                   {"ordinal_position", []},
+                   {"remarks", []},
+                   {"xdbc_data_type", []},
+                   {"xdbc_type_name", []},
+                   {"xdbc_column_size", []},
+                   {"xdbc_decimal_digits", []},
+                   {"xdbc_num_prec_radix", []},
+                   {"xdbc_nullable", []},
+                   {"xdbc_column_def", []},
+                   {"xdbc_sql_data_type", []},
+                   {"xdbc_datetime_sub", []},
+                   {"xdbc_char_octet_length", []},
+                   {"xdbc_is_nullable", []},
+                   {"xdbc_scope_catalog", []},
+                   {"xdbc_scope_schema", []},
+                   {"xdbc_scope_table", []},
+                   {"xdbc_is_autoincrement", []},
+                   {"xdbc_is_generatedcolumn", []}
+                 ]},
+                {"table_constraints",
+                 [
+                   {"constraint_name", []},
+                   {"constraint_type", []},
+                   {"constraint_column_names", []},
+                   {"constraint_column_usage",
+                    [
+                      {"fk_catalog", []},
+                      {"fk_db_schema", []},
+                      {"fk_table", []},
+                      {"fk_column_name", []}
+                    ]}
+                 ]}
+              ]}
+           ],
+           "catalog_name" => []
+         }
+       }} = Connection.get_objects(conn, 0)
     end
   end
 

b5bc26c9bdfe0ea67bf53fd96197d04d6ef6de4c
Cocoa <i@uwucocoa.moe>
2024-05-17 20:31:07 +0100

    fixed metadata

--- c_src/adbc_arrow_array.hpp
+++ c_src/adbc_arrow_array.hpp
@@ -505,7 +505,7 @@ int arrow_array_to_nif_term(ErlNifEnv *env, struct ArrowSchema * schema, struct 
             while (ArrowMetadataReaderRead(&metadata_reader, &key, &value) == NANOARROW_OK) {
                 // printf("key: %.*s, value: %.*s\n", (int)key.size_bytes, key.data, (int)value.size_bytes, value.data);
                 metadata_keys.push_back(erlang::nif::make_binary(env, key.data, (size_t)key.size_bytes));
-                metadata_values.push_back(erlang::nif::make_binary(env, value.data, (size_t)key.size_bytes));
+                metadata_values.push_back(erlang::nif::make_binary(env, value.data, (size_t)value.size_bytes));
             }
             if (metadata_keys.size() > 0) {
                 enif_make_map_from_arrays(env, metadata_keys.data(), metadata_values.data(), (unsigned)metadata_keys.size(), &arrow_metadata);

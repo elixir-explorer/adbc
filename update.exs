6910dd9f71de362248373049cc1d4a482c41e152
Arjun Bajaj <732586+arjunbajaj@users.noreply.github.com>
2024-06-14 22:38:34 +0530

    Upgrade DuckDB to v1.0.0 (#93)

--- update.exs
+++ update.exs
@@ -12,7 +12,7 @@ Mix.install([{:req, "~> 0.4"}])
 defmodule Update do
   # To update duckdb driver, just bump this version
   # https://github.com/duckdb/duckdb/releases/
-  @duckdb_version "0.10.2"
+  @duckdb_version "1.0.0"
 
   # To update ADBC drivers, bump the tag and version accordingly
   # https://github.com/apache/arrow-adbc/releases

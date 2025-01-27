b9694f15429cfe41e2ff2c06eb85483461928383
Cocoa <i@uwucocoa.moe>
2025-01-27 11:52:59 +0100

    include adbc_dll_loader in precompiled binaries (#122)
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- mix.exs
+++ mix.exs
@@ -31,6 +31,7 @@ defmodule Adbc.MixProject do
         make_precompiler_url:
           "#{@github_url}/releases/download/v#{@version}/@{artefact_filename}",
         make_precompiler_filename: "adbc_nif",
+        make_precompiler_priv_paths: ["adbc_nif.*", "adbc_dll_loader.dll"],
         make_precompiler_nif_versions: [versions: ["2.16"]],
         cc_precompiler: [
           cleanup: "clean",

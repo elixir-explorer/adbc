83f820dceed5c27daf02c756422cd37b9b4a543e
Cocoa <i@uwucocoa.moe>
2025-01-28 02:32:01 +0100

    DLLLoaderNif.init
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- lib/adbc_dll_loader_nif.ex
+++ lib/adbc_dll_loader_nif.ex
@@ -4,7 +4,7 @@ defmodule Adbc.DLLLoaderNif do
   # @on_load the order is not guaranteed in a release.
   @moduledoc false
 
-  def add_dll_directory do
+  def init do
     case :os.type() do
       {:win32, _} ->
         priv_dir = :code.priv_dir(:adbc)
